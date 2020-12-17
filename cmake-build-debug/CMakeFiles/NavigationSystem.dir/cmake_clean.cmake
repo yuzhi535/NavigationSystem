931c3bbd53a32c8f80d0174bba0866d0e045037a
yuzhi535 <zhouyuxi.abc@gmail.com>
2020-12-17 21:08:22 +0800

    add something

---
+++ cmake-build-debug/CMakeFiles/NavigationSystem.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../NavigationSystem_zh_CN.ts"
+  "CMakeFiles/NavigationSystem_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/NavigationSystem_autogen.dir/ParseCache.txt"
+  "NavigationSystem_autogen"
+  "CMakeFiles/NavigationSystem.dir/NavigationSystem_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/NavigationSystem.dir/graph.cpp.o"
+  "CMakeFiles/NavigationSystem.dir/graphui.cpp.o"
+  "CMakeFiles/NavigationSystem.dir/main.cpp.o"
+  "CMakeFiles/NavigationSystem.dir/mainwindow.cpp.o"
+  "CMakeFiles/NavigationSystem_zh_CN.ts_lst_file"
+  "NavigationSystem"
+  "NavigationSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NavigationSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
