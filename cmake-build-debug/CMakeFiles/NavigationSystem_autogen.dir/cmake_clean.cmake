09c90b06eeead0e3cb2a9b28bf3672a3033afbc5
yuzhi535 <zhouyuxi.abc@gmail.com>
2020-12-19 23:02:58 +0800

    delete unused files

--- cmake-build-debug/CMakeFiles/NavigationSystem_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/NavigationSystem_autogen"
-  "NavigationSystem_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/NavigationSystem_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
