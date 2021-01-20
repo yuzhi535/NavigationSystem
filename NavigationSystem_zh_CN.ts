dbf858815c6438bdbfa6cdca41947ff568319417
yuzhi535 <zhouyuxi.abc@gmail.com>
2021-01-20 19:26:26 +0800

    add translation

--- NavigationSystem_zh_CN.ts
+++ NavigationSystem_zh_CN.ts
@@ -6,12 +6,14 @@
     <message>
         <location filename="edgedialog.cpp" line="30"/>
         <source>confirm</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>确定</translatorcomment>
+        <translation>确定</translation>
     </message>
     <message>
         <location filename="edgedialog.cpp" line="31"/>
         <source>exit</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>退出</translatorcomment>
+        <translation>退出</translation>
     </message>
 </context>
 <context>
@@ -19,12 +21,13 @@
     <message>
         <location filename="graphui.cpp" line="138"/>
         <source>warning</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>警告</translatorcomment>
+        <translation>警告</translation>
     </message>
     <message>
         <location filename="graphui.cpp" line="139"/>
         <source>no path from %1 to %2</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -36,94 +39,96 @@
         <location filename="out/build/x64-Release/NavigationSystem_autogen/include/ui_mainwindow.h" line="50"/>
         <location filename="out/build/x86-Release/NavigationSystem_autogen/include/ui_mainwindow.h" line="50"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation>主窗口</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="10"/>
         <source>Navigation System</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>导航系统</translatorcomment>
+        <translation>导航系统</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="18"/>
         <source>edit</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>编辑</translatorcomment>
+        <translation>编辑</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="20"/>
         <source>change vertexes&apos; position</source>
-        <translation type="unfinished"></translation>
+        <translation>改变顶点位置</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="21"/>
         <source>change paths and vertexes</source>
-        <translation type="unfinished"></translation>
+        <translation>改边和点</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="37"/>
         <source>graph</source>
-        <translation type="unfinished"></translation>
+        <translation>图</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="45"/>
         <source>output</source>
-        <translation type="unfinished"></translation>
+        <translation>输出</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="51"/>
         <location filename="mainwindow.cpp" line="97"/>
         <location filename="mainwindow.cpp" line="103"/>
         <source>choice</source>
-        <translation type="unfinished"></translation>
+        <translation>选项</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="66"/>
         <source>beginning</source>
-        <translation type="unfinished"></translation>
+        <translation>起点</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="70"/>
         <source>choose the beginning</source>
-        <translation type="unfinished"></translation>
+        <translation>选择起点</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="74"/>
         <source>ending</source>
-        <translation type="unfinished"></translation>
+        <translation>终点</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="78"/>
         <source>choose the ending</source>
-        <translation type="unfinished"></translation>
+        <translation>选择终点</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="96"/>
         <source>people</source>
-        <translation type="unfinished"></translation>
+        <translation>人群</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="102"/>
         <source>car</source>
-        <translation type="unfinished"></translation>
+        <translation>车流</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="115"/>
         <source>confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>确定</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="116"/>
         <source>confirm to find the shortest road</source>
-        <translation type="unfinished"></translation>
+        <translation>确定选择最短路径</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="127"/>
         <source>vertex set</source>
-        <translation type="unfinished"></translation>
+        <translation>顶点集合</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="194"/>
         <source>info</source>
-        <translation type="unfinished"></translation>
+        <translation>信息</translation>
     </message>
 </context>
 <context>
@@ -131,12 +136,12 @@
     <message>
         <location filename="vertexdialog.cpp" line="43"/>
         <source>confirm to change the pos</source>
-        <translation type="unfinished"></translation>
+        <translation>确定改变位置</translation>
     </message>
     <message>
         <location filename="vertexdialog.cpp" line="47"/>
         <source>delete the vertex on the line</source>
-        <translation type="unfinished"></translation>
+        <translation>删除顶点</translation>
     </message>
 </context>
 </TS>
--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 
 #include "graph.h"
 
+#include <QTranslator>
+
 int main(int argc, char *argv[]) {
 	QString fileName("pre.txt");
 	QApplication a(argc, argv);
+
+	QTranslator translator;
+	translator.load("NavigationSystem_zh_CN");
+	a.installTranslator(&translator);
 #if DEV == 1
 	MainWindow w;
 	w.show();
@@ -15,5 +21,7 @@ int main(int argc, char *argv[]) {
 	graph.init_from_file();
 
 #endif
+
+
 	return a.exec();
 }
